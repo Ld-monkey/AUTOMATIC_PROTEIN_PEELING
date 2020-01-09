4945a1f117421a4a9435c346146f3feba37bbd35
lfourteau91@gmail.com <lfourteau91@gmail.com>
2020-01-09 23:36:18 +0100

    Télechargement et extraction du zip dans un dossier

--- automatic_protein_peeling.py
+++ automatic_protein_peeling.py
@@ -5,11 +5,11 @@
 Master II - 2019 2020
 Projet Long
 """
-
-import re
-import robobrowser
+import requests
 import os
+import time
 
+from zipfile import ZipFile 
 from robobrowser import RoboBrowser
 
 if __name__ == "__main__":
@@ -31,5 +31,44 @@ if __name__ == "__main__":
     browser.submit_form(form)
 
     print(browser.parsed)
+
+    TIMER=0
+
+    url = browser.url
+    browser.open(url)
+
+    condition = True
+    while(condition == True):
+        time.sleep(1.0)
+
+        url = browser.url
+        page = browser.parsed
+
+        print("url = ",url)
+        print(TIMER)
+        TIMER = TIMER + 1
+
+        # get links on page
+        links = browser.get_links()
+
+        peeling_path = url[:-12]+"peeling3d.zip"
+        if (requests.head(peeling_path).status_code == 200):
+            print("Exits")
+
+            browser.open(peeling_path)
+
+            with open("result_peeling3d.zip", "wb") as file:
+                file.write(browser.response.content)
+
+            # Extracting zip file.
+            with ZipFile("result_peeling3d.zip", 'r') as zip:
+                # printing all the contents of the zip file
+                zip.printdir()
+
+                # extracting all the files
+                zip.extractall("peeling3d_1qfw_Fv_Human_chorionic_gonadotropin.pdb")
+                print('Extracting Done!')
+            condition = False
+
 
     print("Done")
